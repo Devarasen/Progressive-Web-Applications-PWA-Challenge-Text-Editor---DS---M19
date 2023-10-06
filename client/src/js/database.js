d49e9c5511799112c91c037f1355a27dba8117c4
Devarasen <devarasen@gmail.com>
2023-10-06 23:00:48 +1100

    database implemented

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,39 +3,56 @@ import { openDB } from "idb";
 const DATABASE_NAME = "jate";
 const STORE_NAME = "jate";
 
-const initdb = async () =>
-  openDB(DATABASE_NAME, 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains(STORE_NAME)) {
-        console.log("jate database already exists");
-        return;
-      }
-      db.createObjectStore(STORE_NAME, { keyPath: "id", autoIncrement: true });
-      console.log("jate database created");
-    },
-  });
+const initdb = async () => {
+  try {
+    await openDB(DATABASE_NAME, 1, {
+      upgrade(db) {
+        if (db.objectStoreNames.contains(STORE_NAME)) {
+          console.log("jate database already exists");
+          return;
+        }
+        db.createObjectStore(STORE_NAME, {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+        console.log("jate database created");
+      },
+    });
+  } catch (error) {
+    console.error("Error initializing database:", error);
+  }
+};
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log("PUT to the database");
-  const todosDb = await openDB(DATABASE_NAME, 1);
-  const tx = todosDb.transaction(STORE_NAME, "readwrite");
-  const store = tx.objectStore(STORE_NAME);
-  const request = store.put({ id: id, todo: content });
-  const result = await request;
-  console.log("Data saved to the database", result);
+  try {
+    console.log("PUT to the database");
+    const todosDb = await openDB(DATABASE_NAME, 1);
+    const tx = todosDb.transaction(STORE_NAME, "readwrite");
+    const store = tx.objectStore(STORE_NAME);
+    const result = await store.put({ todo: content });
+    console.log("Data saved to the database", result);
+    todosDb.close();
+  } catch (error) {
+    console.error("Error saving to database:", error);
+  }
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log("GET all from the database");
-  const todosDb = await openDB(DATABASE_NAME, 1);
-  const tx = todosDb.transaction(STORE_NAME, "readonly");
-  const store = tx.objectStore(STORE_NAME);
-  const request = store.getAll();
-  const result = await request;
-  console.log("result.value", result);
-  return result;
+  try {
+    console.log("GET all from the database");
+    const todosDb = await openDB(DATABASE_NAME, 1);
+    const tx = todosDb.transaction(STORE_NAME, "readonly");
+    const store = tx.objectStore(STORE_NAME);
+    const result = await store.getAll();
+    console.log("result.value", result);
+    todosDb.close();
+    return result;
+  } catch (error) {
+    console.error("Error getting data from database:", error);
+    throw error;
+  }
 };
 
 initdb();
